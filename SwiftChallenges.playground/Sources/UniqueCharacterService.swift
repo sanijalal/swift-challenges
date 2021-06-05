753a060c0a05a04eddb53a6b1643816341fd1747
Abd Sani <binabdjalal@gmail.com>
2021-06-05 12:36:10 +0800

    Create separate files. But test suite is removed. This one is easier for the eyes but I don't have a test suite anymore.

---
+++ SwiftChallenges.playground/Sources/UniqueCharacterService.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public struct UniqueCharacterService {
+   public func areCharactersUnique(_ inputString: String) -> Bool {
+        for (index, char) in inputString.enumerated() {
+            let stringIndex = inputString.index(inputString.startIndex, offsetBy: index)
+            var stringToCut = inputString
+            stringToCut.remove(at: stringIndex)
+            if stringToCut.contains(char) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    public init() {
+        
+    }
+}
