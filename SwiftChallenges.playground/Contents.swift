c49e7f3b339512454ef1e5d598966843469284a5
Abd Sani <binabdjalal@gmail.com>
2021-06-05 11:08:11 +0800

    Add tests and function to test

--- SwiftChallenges.playground/Contents.swift
+++ SwiftChallenges.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+import XCTest
 
-var str = "Hello, playground"
+struct UniqueCharacterService {
+    func areCharactersUnique(_ inputString: String) -> Bool {
+        return false
+    }
+}
+
+class UniqueCharacterTests: XCTestCase {
+    var service: UniqueCharacterService!
+    
+    override func setUp() {
+        super.setUp()
+        service = UniqueCharacterService()
+    }
+    
+    func testValidString() {
+        let isUnique = service.areCharactersUnique("abc")
+        XCTAssertEqual(isUnique, true)
+    }
+    
+    func testInvalidString() {
+        let isUnique = service.areCharactersUnique("xx")
+        XCTAssertEqual(isUnique, false)
+    }
+}
+
+UniqueCharacterTests.defaultTestSuite.run()
